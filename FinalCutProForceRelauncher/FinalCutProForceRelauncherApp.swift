331d81ffa58150ad8713e9968059ff02baa60543
Tomohiro Kumagai <tomo@ez-net.jp>
2022-09-25 23:22:50 +0900

    Initial Commit

---
+++ FinalCutProForceRelauncher/FinalCutProForceRelauncherApp.swift
@@ -0,0 +1,18 @@
+//
+//  FinalCutProForceRelauncherApp.swift
+//  FinalCutProForceRelauncher
+//  
+//  Created by Tomohiro Kumagai on 2022/09/25
+//  
+//
+
+import SwiftUI
+
+@main
+struct FinalCutProForceRelauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
